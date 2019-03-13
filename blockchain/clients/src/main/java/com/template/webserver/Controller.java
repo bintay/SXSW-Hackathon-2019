79d1f3a6116ad7c22ed35702ba2eda6512bad2fe
Ben Sun <praeceptorexsaecula@gmail.com>
2019-03-12 23:01:31 -0500

    POST stuff

--- blockchain/clients/src/main/java/com/template/webserver/Controller.java
+++ blockchain/clients/src/main/java/com/template/webserver/Controller.java
@@ -2,17 +2,25 @@ package com.template.webserver;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.template.IssueVideoFlow;
 import com.template.VideoState;
 
 import net.corda.core.contracts.StateAndRef;
+import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
 
 /**
@@ -38,6 +46,16 @@ public class Controller {
         return proxy.nodeInfo().getLegalIdentities().get(0).toString();
     }
     
+    @PostMapping(path = "issue-video", produces = "text/plain")
+    private ResponseEntity<String> issueAsset(HttpServletRequest request) throws ExecutionException, InterruptedException {
+        String identifier = request.getParameter("identifier");
+        String similarityScoreString = request.getParameter("similarityScore");
+        Double similarityScore = Double.valueOf(similarityScoreString);
+
+        proxy.startFlowDynamic(IssueVideoFlow.class, identifier, similarityScore).getReturnValue().get();
+        return ResponseEntity.status(HttpStatus.CREATED).body("Video issued.\n");
+    }
+    
     @GetMapping(value = "infojson", produces = "text/plain")
     private String infojson() {
         List<StateAndRef<VideoState>> states = proxy.vaultQuery(VideoState.class).getStates();
@@ -45,7 +63,7 @@ public class Controller {
             VideoState Video = stateAndRef.getState().getData();
 
             HashMap<String, String> map = new HashMap<>();
-            map.put("Band_Name", Video.getIdentifier());
+            map.put("Band Name", Video.getIdentifier());
             map.put("Score", String.valueOf(Video.getScore()));
 
             return map;
@@ -53,10 +71,10 @@ public class Controller {
         String end = "[\n\t";
         if (info.size() > 0) {
         		for (int x = 0; x < info.size()-1; x++) {
-        			end += "{\n\t\t\"Band_Name\": \"" + info.get(x).get("Band Name") + "\",\n";
+        			end += "{\n\t\t\"Band Name\": \"" + info.get(x).get("Band Name") + "\",\n";
       	 		end += "\t\t\"Score\": " + info.get(x).get("Score") + "\n\t},";
         		}
-        		end += "\n\t{\n\t\t\"Band_Name\": \"" + info.get(info.size()-1).get("Band Name") + "\",\n";
+        		end += "\n\t{\n\t\t\"Band Name\": \"" + info.get(info.size()-1).get("Band Name") + "\",\n";
       	 	end += "\t\t\"Score\": " + info.get(info.size()-1).get("Score") + "\n\t}";
         }
         end+= "\n]";
