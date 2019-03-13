3a7e3bea11bf7a3187793ff40b09bcf6af398486
Ben Taylor <bentaylor@bentaylor.me>
2019-03-12 22:45:39 -0500

    Almost done with front end

--- frontend/main.js
+++ frontend/main.js
@@ -19,21 +19,46 @@ app.get('/', function (req, res) {
    res.sendfile(__dirname + '/view/index.html');
 });
 
-app.post('/submit/', type, function (req, res) {
+app.post('/submit/:name/:email/', type, function (req, res) {
    res.setHeader("Access-Control-Allow-Origin", "*");
    let name = uuid();
+   console.log(req.params.name);
+   console.log(req.files.file);
    
    let file = req.files.file;
    file.mv(__dirname + '/public/uploads/' + name, function(err) {
-      if (err)
+      if (err) {
          return res.status(500).send(err);
-
-      res.json({
-         filename: name
-      });
+      } else {
+         getFeedback (name, function (tips, score) {
+            res.json({
+               score: score,
+               tips: tips
+            });
+         });
+      }
    });
 });
 
 app.listen(PORT, function () {
    console.log(`Listening on port ${PORT}`);
 });
+
+function getTranslation (sentence, language, callback) {
+   exec(`export GOOGLE_APPLICATION_CREDENTIALS=/Users/bent/gcloud/Hacklahoma-2019-cd5a0529ed91.json && curl -X POST \\
+      -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \\
+      -H "Content-Type: application/json; charset=utf-8" \\
+      --data "{
+         'q': '${sentence.replace(/'/, "\\'")}',
+         'target': '${language}'
+      }" "https://translation.googleapis.com/language/translate/v2"`, (err, stdout, stderr) => {
+      if (err) console.log(err);
+      callback(JSON.parse(stdout));
+   });
+}
+
+function getFeedback (fileurl, callback) {
+   setTimeout(() => {
+      callback(["marketing on Spotify in Mexico"], Math.random());
+   }, 2000)
+}
